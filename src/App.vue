b7026c9c3619f7771cae0d9bd9230295debd44f2
zhangshibo <1746436279@qq.com>
2023-08-27 16:52:11 +0800

    feat: element-plus集成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1>我是app根组件</h1>
+    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
+    <el-button type="danger" size="small" :icon="Delete">编辑按钮</el-button>
+    <el-pagination
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    />
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//@ts-ignore
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+</script>
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-expect-error 忽略当前ts类型的检测否则有红色提示打包失败
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
-
-const fun = () => {
-  console.log('测试push')
-}
-
-fun()
+//获取应用实例对象
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+//将应用挂载到挂载点上
+app.mount('#app')
