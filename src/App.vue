2fb2d40bfdc09007ea0d6c8012dca4c09a4ed686
zhangshibo <1746436279@qq.com>
2023-08-29 01:44:12 +0800

    chore: axios二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h1>测试代码</h1>
+    <h1>测试axios二次封装</h1>
   </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss">
-h1 {
-  color: $color;
-}
-</style>
+<script setup lang="ts">
+import request from '@/utils/request.ts'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+})
+</script>
+<style scoped></style>
