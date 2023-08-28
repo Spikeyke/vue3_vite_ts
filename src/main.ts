f782e51ff294835b441faf47993b3fc898ef0fb1
zhangshibo <1746436279@qq.com>
2023-08-29 01:10:17 +0800

    chore: 集mock接口

--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,20 @@ import globalComponent from '@/components'
 app.use(globalComponent)
 //引入模板的全局的样式
 import '@/styles/index.scss'
+
+/*
+ * 测试假接口能否使用
+ * */
+import axios from 'axios'
+//登录接口
+axios({
+  url: '/api/user/login',
+  method: 'post',
+  data: {
+    username: 'admin',
+    password: '111111',
+  },
+})
+
 //将应用挂载到挂载点上
 app.mount('#app')
