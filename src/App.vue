ee8d537c9f5b1beb19de047e9295c8429c26ded3
zhangshibo <1746436279@qq.com>
2023-08-30 02:08:09 +0800

    chore: api接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div>
-    <h1>测试axios二次封装</h1>
+    <h1>根组件</h1>
   </div>
 </template>
 <script setup lang="ts">
-import request from '@/utils/request.ts'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
+import{onMounted}from 'vue'
+import {reqLogin} from "@/api/user";
+onMounted(()=>{
+  reqLogin({username:'admin',password:'111111'})
 })
 </script>
 <style scoped></style>
