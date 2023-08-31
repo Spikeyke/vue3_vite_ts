f6f356cf4bcb38ceba1781e96d2d3445d17a5b41
Spikeyke <1746436279@qq.com>
2023-08-31 14:16:41 +0800

    feat: 用户仓库数据ts类型的定义

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,23 +1,26 @@
 //创建用户相关的小仓库
 import { defineStore } from 'pinia'
 import { reqLogin } from '@/api/user'
-import { loginForm } from '@/api/user/type.ts'
+import { loginForm, loginResponseData } from '@/api/user/type.ts'
+import { UserState } from '@/store/modules/types/type.ts'
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token.ts'
 
-let useUserStore = defineStore('User', {
+const useUserStore = defineStore('User', {
   //小仓库存储数据
-  state: () => {
+  state: (): UserState => {
     return {
-      token: localStorage.getItem('TOKEN'),
+      token: GET_TOKEN(),
     }
   },
   //异步|逻辑
   actions: {
     async userLogin(data: loginForm) {
-      let result: any = await reqLogin(data)
+      const result: loginResponseData = await reqLogin(data)
       if (result.code == 200) {
         //pinia|vuex存储数据其实利用js对象，并不持久化
-        this.token = result.data.token
-        localStorage.setItem('TOKEN', result.data.token)
+        this.token = result.data.token as string
+        // localStorage.setItem('TOKEN', (result.data.token as string))
+        SET_TOKEN(result.data.token as string)
         return 'ok'
       } else {
         return Promise.reject(new Error(result.data.message))
