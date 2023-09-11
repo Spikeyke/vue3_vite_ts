99fe29d2d9527e739554699ac75ad365631f0115
zhangshibo <1746436279@qq.com>
2023-09-12 00:06:31 +0800

    feat: 左侧菜单静态的搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,48 +1,70 @@
 <template>
-  <div class="layout_container">
-    <!--    左侧菜单-->
-    <div class="layout_slider">
-      <Logo></Logo>
+    <div class="layout_container">
+        <!--    左侧菜单-->
+        <div class="layout_slider">
+            <Logo></Logo>
+            <!-- 滚动菜单 -->
+            <!-- 滚动组件 -->
+            <el-scrollbar class="scrollbar">
+                <!-- 菜单组件 -->
+                <el-menu background-color="#001529" text-color="white">
+                    <el-menu-item index="1">首页</el-menu-item>
+                    <el-menu-item index="2">数据大屏</el-menu-item>
+                    <!-- 折叠菜单 -->
+                    <el-sub-menu index=3>
+                        <template #title>权限管理</template>
+                        <el-menu-item index="3-1">用户管理</el-menu-item>
+                        <el-menu-item index="3-2">角色管理</el-menu-item>
+                        <el-menu-item index="3-3">菜单管理</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </el-scrollbar>
+        </div>
+        <!--    顶部导航-->
+        <div class="layout_tabbar"></div>
+        <!--    内容展示区域-->
+        <div class="layout_main"></div>
     </div>
-    <!--    顶部导航-->
-    <div class="layout_tabbar"></div>
-    <!--    内容展示区域-->
-    <div class="layout_main"></div>
-  </div>
 </template>
 
 <script setup lang="ts">
-  import Logo from '@/layout/logo/index.vue'
+import Logo from '@/layout/logo/index.vue'
 </script>
 <style scoped lang="scss">
 .layout_container {
-  width: 100%;
-  height: 100vh;
-  background: rgb(24, 136, 151);
-
-  .layout_slider {
-    background: $base-menu-background;
-    width: $base-menu-width;
+    width: 100%;
     height: 100vh;
-  }
+    background: rgb(24, 136, 151);
 
-  .layout_tabbar {
-    position: fixed;
-    width: calc(100% - $base-menu-width);
-    height: $base-tabbar-height;
-    background: cyan;
-    top: 0px;
-    left: $base-menu-width;
-  }
+    .layout_slider {
+        background: $base-menu-background;
+        width: $base-menu-width;
+        height: 100vh;
+        .scrollbar{
+            width: 100%;
+            height: calc(100vh - $base-menu-logo-height);
+            color: white;
+        }
+    }
 
-  .layout_main {
-    position: absolute;
-    width: calc(100% - $base-menu-width);
-    height: calc(100vh - $base-tabbar-height);
-    background: yellowgreen;
-    left: $base-menu-width;
-    top: $base-tabbar-height;
-    padding: 20px;
-    overflow: auto;
-  }
-}</style>
+    .layout_tabbar {
+        position: fixed;
+        width: calc(100% - $base-menu-width);
+        height: $base-tabbar-height;
+        background: cyan;
+        top: 0px;
+        left: $base-menu-width;
+    }
+
+    .layout_main {
+        position: absolute;
+        width: calc(100% - $base-menu-width);
+        height: calc(100vh - $base-tabbar-height);
+        background: yellowgreen;
+        left: $base-menu-width;
+        top: $base-tabbar-height;
+        padding: 20px;
+        overflow: auto;
+    }
+}
+</style>
