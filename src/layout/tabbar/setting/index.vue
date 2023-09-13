00030f614bd7110051bd327de6d5b2b8f75b156f
Spikeyke <1746436279@qq.com>
2023-09-13 15:02:44 +0800

    feat: 获取用户信息与token

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -2,11 +2,11 @@
   <el-button size="small" icon="Refresh" circle @click="updateRefresh"></el-button>
   <el-button size="small" icon="FullScreen" circle @click="fullScreen"></el-button>
   <el-button size="small" icon="Setting" circle></el-button>
-  <img src="../../../../public/logo.png" style="width: 24px; height: 24px; margin: 0px 10px" />
+  <img :src="userStore.avatar" style="width: 24px; height: 24px; margin: 0px 10px;border-radius: 50%;" />
   <!-- 下拉菜单 -->
   <el-dropdown style="margin-right: 10px">
     <span class="el-dropdown-link">
-      admin
+      {{ userStore.username }}
       <el-icon class="el-icon--right">
         <arrow-down />
       </el-icon>
@@ -20,9 +20,12 @@
 </template>
 
 <script setup lang="ts">
+//获取用户的小仓库
+import useUserStore from '@/store/modules/user';
 //获取骨架的小仓库
 import useLayoutSettingStore from '@/store/modules/setting';
 let layoutSettingStore = useLayoutSettingStore()
+let userStore = useUserStore()
 const updateRefresh = () => {
   layoutSettingStore.refresh = !layoutSettingStore.refresh
 }
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,6 +1,6 @@
 //创建用户相关的小仓库
 import { defineStore } from 'pinia'
-import { reqLogin } from '@/api/user'
+import { reqLogin, reqUserInfo } from '@/api/user'
 import { loginForm, loginResponseData } from '@/api/user/type.ts'
 import { UserState } from '@/store/modules/types/type.ts'
 import { SET_TOKEN, GET_TOKEN } from '@/utils/token.ts'
@@ -13,10 +13,13 @@ const useUserStore = defineStore('User', {
     return {
       token: GET_TOKEN(),
       menuRoutes: constantRoute, //仓库存储生成菜单需要数组（路由）
+      username: '',
+      avatar: ''
     }
   },
   //异步|逻辑
   actions: {
+    //用户登录的方法
     async userLogin(data: loginForm) {
       const result: loginResponseData = await reqLogin(data)
       if (result.code == 200) {
@@ -29,6 +32,18 @@ const useUserStore = defineStore('User', {
         return Promise.reject(new Error(result.data.message))
       }
     },
+    //获取用户信息方法
+    async userInfo() {
+      //获取用户信息进行存储仓库当中[用户头像、名字
+      let result = await reqUserInfo()
+      //如果获取用户信息成功，存储一下用户信息
+      if (result.code === 200) {
+        this.username = result.data.checkUser.username
+        this.avatar = result.data.checkUser.avatar
+      } else {
+
+      }
+    }
   },
   getters: {},
 })
