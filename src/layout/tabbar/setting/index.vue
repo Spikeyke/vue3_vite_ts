b26e19acea67006e66f680bc740ed42e20103bb1
Spikeyke <1746436279@qq.com>
2023-09-13 14:17:14 +0800

    feat: 全屏模式的切换

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,11 +1,8 @@
 <template>
   <el-button size="small" icon="Refresh" circle @click="updateRefresh"></el-button>
-  <el-button size="small" icon="FullScreen" circle></el-button>
+  <el-button size="small" icon="FullScreen" circle @click="fullScreen"></el-button>
   <el-button size="small" icon="Setting" circle></el-button>
-  <img
-    src="../../../../public/logo.png"
-    style="width: 24px; height: 24px; margin: 0px 10px"
-  />
+  <img src="../../../../public/logo.png" style="width: 24px; height: 24px; margin: 0px 10px" />
   <!-- 下拉菜单 -->
   <el-dropdown style="margin-right: 10px">
     <span class="el-dropdown-link">
@@ -23,12 +20,21 @@
 </template>
 
 <script setup lang="ts">
-  //获取骨架的小仓库
-  import useLayoutSettingStore from '@/store/modules/setting';
-  let layoutSettingStore=useLayoutSettingStore()
-  const updateRefresh=()=>{
-    layoutSettingStore.refresh=!layoutSettingStore.refresh
+//获取骨架的小仓库
+import useLayoutSettingStore from '@/store/modules/setting';
+let layoutSettingStore = useLayoutSettingStore()
+const updateRefresh = () => {
+  layoutSettingStore.refresh = !layoutSettingStore.refresh
+}
+const fullScreen = () => {
+  //DOM对象的一个属性：可以用来判断当前是不是全屏模式[全屏：true，不是全屏：false]
+  let full = document.fullscreenElement
+  if (!full) {
+    document.documentElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
   }
+}
 </script>
 <script lang="ts">
 export default {
@@ -36,4 +42,3 @@ export default {
 }
 </script>
 <style lang="scss" scoped></style>
-@/store/modules/setting
