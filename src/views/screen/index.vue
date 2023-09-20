19fb9112c71c3762a7c42c773010fd90ac5b4c8d
Spikeyke <1746436279@qq.com>
2023-09-20 18:04:57 +0800

    feat: 数据大屏顶部搭建

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <div class="">
-    <p>数据大屏一级路由组件</p>
+  <div class="container">
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top></Top>
+      </div>
+      <div class="bottom">
+        <div class="left"></div>
+        <div class="center"></div>
+        <div class="right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import Top from './components/top/index.vue'
 
-<style lang="scss" scoped></style>
+let screen = ref()
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+
+//定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+
+//监听视口变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url("./images/bg.png") no-repeat;
+  background-size: cover;
+
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    transform-origin: left top;
+
+    .top {
+      height: 40px;
+    }
+
+    .bottom {
+      display: flex;
+
+      .left {
+        flex: 1;
+      }
+
+      .right {
+        flex: 1;
+      }
+
+      .center {
+        flex: 2;
+      }
+    }
+  }
+}
+</style>
