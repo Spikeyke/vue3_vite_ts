46f48c93550fc307414d1a30010fb5546fc4a27b
zhangshibo <1746436279@qq.com>
2023-09-21 23:58:53 +0800

    feat: 数据大屏实时游客统计

---
+++ src/views/screen/components/tourist/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="box">
+    <div class="top">
+      <p class="title">实时游客统计</p>
+      <p class="bg"></p>
+      <p class="right">可预约总量<span>9999999</span>人</p>
+    </div>
+    <div class="number">
+      <span v-for="(item,index) in people" :key="index">{{ item }}</span>
+    </div>
+<!--    echarts展示图形的节点-->
+    <div class="echarts"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+
+let people = ref('216908人')
+</script>
+
+<style lang="scss" scoped>
+.box {
+  background: url('../../images/dataScreen-main-lb.png') no-repeat;
+  background-size: 100% 100%;
+
+  .top {
+    margin-left: 20px;
+
+    .title {
+      color: white;
+      font-size: 20px;
+    }
+
+    .bg {
+      width: 68px;
+      height: 7px;
+      background: url('../../images/dataScreen-title.png') no-repeat;
+      background-size: 100% 100%;
+      margin-top: 10px;
+    }
+
+    .right {
+      float: right;
+      color: white;
+      font-size: 15px;
+
+      span {
+        color: lightgoldenrodyellow;
+      }
+    }
+  }
+
+  .number {
+    display: flex;
+    margin:  40px 20px 0 30px;
+    span {
+      flex: 1;
+      height: 50px;
+      text-align: center;
+      line-height: 50px;
+      background: url('../../images/total.png') no-repeat;
+      background-size: 100% 100%;
+      color: #29fcff;
+    }
+  }
+  .echarts{
+    width:100%;
+    height: 250px;
+  }
+}
+</style>
