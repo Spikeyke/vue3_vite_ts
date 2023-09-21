40eb174cc1c5ce98134c928a1a807d3719bde881
zhangshibo <1746436279@qq.com>
2023-09-22 00:15:55 +0800

    feat: 水球图完成

--- src/views/screen/components/tourist/index.vue
+++ src/views/screen/components/tourist/index.vue
@@ -9,14 +9,40 @@
       <span v-for="(item,index) in people" :key="index">{{ item }}</span>
     </div>
 <!--    echarts展示图形的节点-->
-    <div class="echarts"></div>
+    <div class="echarts" ref="charts"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
-
+import {ref,onMounted} from 'vue'
+import * as echarts from 'echarts'
+import 'echarts-liquidfill'
 let people = ref('216908人')
+//获取节点
+let charts=ref()
+onMounted(()=>{
+  //获取echarts类的实例
+  let mycharts =echarts.init(charts.value)
+  //设置实例的配置项
+  mycharts.setOption({
+    series:{
+      type:'liquidFill',
+      data:[0.6,0.4],
+      radius:'100%',
+      outline: {//外层边框颜色设置
+        show: true,
+        borderDistance: 8,
+        itemStyle: {
+          color: 'skyblue',
+          borderColor: '#294D99',
+          borderWidth: 8,
+          shadowBlur: 20,
+          shadowColor: 'rgba(0, 0, 0, 0.25)'
+        }
+      },
+    }
+  })
+})
 </script>
 
 <style lang="scss" scoped>
